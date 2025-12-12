fc7102dcdbe6e65742f086aba55c4daf6388e131
Marc Schopper <marc@schopper.rocks>
2025-12-12 17:52:36 +0100

    Use local Tailwind build

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./en/*.html",
+    "./tests/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
